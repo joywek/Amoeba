937bb0d58fbdd980c74020fb2baf1793d9113943
wenbi <wenbi@alipay.com>
2017-02-09 17:40:39 +0800

    新增关于页面的模板

---
+++ page-about.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * The template for displaying about page.
+ *
+ * @package Amoeba
+ */
+
+get_header(); ?>
+
+<div id="site-body">
+	<div class="sidebar">
+		<aside class="widget widget-info">
+			<div class="avatar">
+			</div>
+			<h2>Amit</h2>
+			<h6>专注移动软件架构</h6>
+		</aside>
+		<aside class="widget widget-profile">
+			<header class="widget-header">
+				<h4 class="title">Profile</h4>
+			</header>
+			<p>应该学会用一种文学的方法描写自己，在某一个自己最得意的特点上，下大功夫描写自己，当然，也不要企图掩盖自己的不足，所谓君子坦荡荡，小人长戚戚，只有通过你的自我介绍，看到一个立体的生动的磊落的你，这才说明你的自我介绍或者说自画像是成功的。</p>
+		</aside>
+		<aside class="widget widget-contact">
+			<header class="widget-header">
+				<h4 class="title">联系信息</h4>
+			</header>
+			<ul class="widget-body">
+				<li>
+					<div class="title">Phone</div>
+					<div class="text">18611083587</div>
+				</li>
+				<li>
+					<div class="title">Email:</div>
+					<div class="text">joywek@gmail.com</div>
+				</li>
+			</ul>
+		</aside>
+	</div>
+	<div class="main">
+		<div class="inner">
+			<div class="section experience">
+				<h4 class="section-header">
+					<span>Experience</span>
+				</h4>
+				<p>Clean Resume Word Template. Available in all format – Photoshop, Illustrator, Indesign & MS Word. Elegant page designs are easy to use and customize, so you can quickly tailor-make your resume for any opportunity and help you to get your job.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
