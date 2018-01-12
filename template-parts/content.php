999f173897cd8ca0e1043512c7213ac127acf75d
wenbi <wenbi@alipay.com>
2018-01-12 11:37:39 +0800

    在文章详情页去掉标题上的链接

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,7 +1,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-entry'); ?>>
 	<div class="inner">
 		<header class="entry-header">
+		<?php if (!is_single()) : ?>
 			<h1><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+		<?php else : ?>
+			<h1><?php the_title() ?></h1>
+		<?php endif ?>
+
 			<div class="entry-meta">
 				<div class="inner"><?php amoeba_entry_meta(); ?></div>
 			</div>
